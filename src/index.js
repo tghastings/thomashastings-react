0c80ae1462d213da2eab201bdafba5997857c5ac
thomas hastings <thomas@thomashastings.com>
2019-11-22 17:06:25 -0700

    Init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Posts from "./Posts"
+import Post from "./Post"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={Posts} />
+      <Route path="/post/:id" component={Post} />
+    </div>
+  </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routing, document.querySelector("#root"));
